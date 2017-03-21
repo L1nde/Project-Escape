dc6dc3fbce618cdfbddb2af9c58155257972480d
L1ND3 <einar.linde.001mail.ee>
2017-03-21 15:34:02 +0200

    Server<->Client Terrible lag, but works. Very very pre-alpha

--- src/server/Server.java
+++ src/server/Server.java
@@ -5,6 +5,8 @@ import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
 
 /**
  * Created by Meelis Perli on 3/18/2017.
@@ -12,12 +14,16 @@ import java.util.List;
 public class Server {
     public static void main(String[] args) throws IOException {
         List<Thread> threadList = new ArrayList<>();
+        List<BlockingQueue<String>> data = new ArrayList<>();
+        int id = -1;
         try (ServerSocket ss = new ServerSocket(1337)) {
             while (true) {
                 try {
                     Socket sock = ss.accept();
-                    threadList.add(new Thread(new ServerMain(sock)));
+                    data.add(new ArrayBlockingQueue<>(10));
+                    threadList.add(new Thread(new ServerMain(sock, data, id++)));
                     threadList.get(threadList.size()-1).start();
+
 
                 } catch (IOException e) {
                     System.out.println(e.getMessage()); // nii ikka võis teha?
