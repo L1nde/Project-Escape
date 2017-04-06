41e3642f12107e10e0a997c9fdf38274526f88cd
L1ND3 <einar.linde.001mail.ee>
2017-04-06 12:30:32 +0300

    Food textures

--- src/main/java/client/entities/Food.java
+++ src/main/java/client/entities/Food.java
@@ -1,9 +1,9 @@
 package client.entities;
 
 import client.MapObjects;
-import org.newdawn.slick.Color;
-import org.newdawn.slick.GameContainer;
-import org.newdawn.slick.Graphics;
+import org.newdawn.slick.*;
+import org.newdawn.slick.geom.Circle;
+import org.newdawn.slick.geom.Rectangle;
 import org.newdawn.slick.state.StateBasedGame;
 
 /**
@@ -13,14 +13,22 @@ public class Food implements MapObjects{
 
     private final int[] cords;
     private final int radius = 5;
+    private Image foodTexture;
 
     public Food(int[] cords) {
         this.cords = cords;
+        try {
+            this.foodTexture = new Image("resources/PizzaTexture.png");
+        } catch (SlickException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
     public void render(GameContainer container, StateBasedGame game, Graphics g) {
-        g.setColor(Color.blue);
-        g.fillOval(cords[0] + 10 - radius/2, cords[1] + 10 - radius/2, radius, radius);
+        g.setColor(Color.white);
+//        g.texture(new Rectangle(cords[0]+4, cords[1]+6, 10, 5), foodTexture, 1, 1, true);
+        g.texture(new Circle(cords[0] + 10 - radius/2, cords[1] + 10 - radius/2, radius, radius), foodTexture, 1, 1, true);
+//        g.fillOval(cords[0] + 10 - radius/2, cords[1] + 10 - radius/2, radius, radius);
     }
 }
